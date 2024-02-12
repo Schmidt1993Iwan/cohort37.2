690368f8a6544a4df96d9658588b92b4c3ca67b5
Schmidt1993iwan <schmidt1993iwan@gmail.com>
2024-02-13 00:04:44 +0100

    homework_01_02

---
+++ front_end/lesson_08/homework_02/index.js
@@ -0,0 +1,35 @@
+const a = 17;
+const b = 5;
+
+console.log(a > b);
+console.log(a <= b);
+console.log(a == b);
+console.log(a != b);
+console.log("================")
+console.log('a' < 'b');
+console.log('ab' > 'a');
+console.log("================")
+console.log('17' > 1);
+console.log(17 === 1);
+console.log(17 === 17);
+console.log("================")
+console.log(17 === '17');
+console.log(17 === true);
+console.log('0' === '');
+console.log("================")
+console.log(true === false);
+console.log(true === true);
+console.log(null === undefined);
+console.log(false === 0);
+console.log("================")
+console.log(17 == '17');
+console.log('0' == '');
+console.log(0 == '');
+console.log(null == undefined);
+console.log(false == 0);
+console.log("================")
+console.log(undefined == null);
+console.log(undefined == 0);
+console.log(null == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
